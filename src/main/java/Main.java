ef971e5595d20a7474e2727c2a295f6383d07573
Evija <evija_f@inbox.lv>
2021-02-28 20:42:40 +0200

    Majasdarbs

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Hello! My name is Evija Abolina.");
+        System.out.println("I live in Kuldiga for 11 years.");
+        System.out.println("Before I lived in Liepaja.");
+        System.out.println("I graduated high school at 2009.");
+        System.out.println("Then I studied as a secretary and " +
+                "started to work at this field for few years.");
+        System.out.println("Then I ended up in pharmacy as a pharmacists helper.");
+        System.out.println("Since then nothing has changed.");
+        System.out.println("I am still a pharmacists helper but now" +
+                " I am ready for a new challenges.");
+        System.out.println("I want to enter in IT field as" +
+                " a Java developer.");
+    }
+}
